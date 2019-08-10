ee7eff81b044a54ba7dd5b9e5a42dd667beedcfb
Dylan Thinnes <dylan.thinnes@protonmail.com>
2019-08-10 16:24:57 +0100

    Fix toc.sh to detect largest header

--- get/article/toc.sh
+++ get/article/toc.sh
@@ -2,12 +2,25 @@ echo '<div class="toc">'
 echo '<h3>Table of Contents</h3>'
 
 ./get/body.sh $1 | ./utils/markdown.sh | perl -e '''
-while (<>) {
-    if (m/<h(\d)[^>]*id="([^"]+)"[^>]*>(.+)<\/h\1>/) {
-        print " "x(($1-2)*4);
-        print "* ";
-        print "[${3}](#${2})";
-        print "\n";
+$_ = join "\n", <>;
+$i = 0;
+$m = 100;
+for my $line (split /\n/, $_) {
+    if ($line =~ m/<h(\d)[^>]*id="([^"]+)"[^>]*>(.+)<\/h\1>/) {
+        $i += 1;
+        if ($m > $1) {
+            $m = $1;
+        }
+    }
+}
+if ($i > 0) {
+    for my $line (split /\n/, $_) {
+        if ($line =~ m/<h(\d)[^>]*id="([^"]+)"[^>]*>(.+)<\/h\1>/) {
+            print " "x(($1-$m)*4);
+            print "* ";
+            print "[${3}](#${2})";
+            print "\n";
+        }
     }
 }
 ''' | ./utils/markdown.sh
