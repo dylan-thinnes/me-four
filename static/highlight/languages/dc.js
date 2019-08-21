24f4515bf99565362dc2c0c3cf16c66cedb998b4
Dylan Thinnes <dylan.thinnes@protonmail.com>
2019-08-21 20:21:28 +0100

    Enhance dc highlighting to have repl-mode

--- static/highlight/languages/dc.js
+++ static/highlight/languages/dc.js
@@ -1,31 +1,49 @@
+var dcComment = hljs.COMMENT("#","$");
+var dcSpec = [
+    dcComment,
+    {
+        className: 'string',
+        begin: '\\[[^\\n]*\\]',
+        end: ''
+    },
+    {
+        className: 'no_style',
+        begin: '{',
+        end: '}'
+    },
+    {
+        className: 'number',
+        begin: '[0-9A-F]+',
+        end: ''
+    },
+    {
+        className: 'keyword',
+        begin: '[lLsS].',
+        end: '',
+    },
+    {
+        className: 'built_in',
+        begin: '[xpfcdr?+\\-*/%~^|v]',
+        end: '',
+    }
+]
 hljs.registerLanguage('dc', function (hljs) {
     return {
         aliases: ['dc'],
         case_insensitive: false,
+        contains: dcSpec
+    }
+});
+hljs.registerLanguage('dc-repl', function (hljs) {
+    return {
+        aliases: ['dc-repl'],
+        case_insensitive: false,
         contains: [
-            hljs.COMMENT(
-                "#",
-                "$"
-            ),
+            dcComment,
             {
-                className: 'no_style',
-                begin: '{',
-                end: '}'
-            },
-            {
-                className: 'number',
-                begin: '[0-9A-F]',
-                end: '[^0-9A-F]'
-            },
-            {
-                className: 'keyword',
-                begin: '[lLsS].',
-                end: '',
-            },
-            {
-                className: 'built_in',
-                begin: '[xpfcdr?+\\-*/%~^|v]+',
-                end: '[^xpfcdr?+\\-*/%~^|v]',
+                begin: '(> |  )',
+                end: '\n',
+                contains: dcSpec
             }
         ]
     }
