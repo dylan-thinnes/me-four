d2b45ba8ac912627c8ff0f63e22cae06fd95ae6e
Dylan Thinnes <dylan.thinnes@protonmail.com>
2019-09-03 21:15:02 +0100

    Update Prism `dc` definition from `dc-repl`

--- static/prism/components/prism-dc.js
+++ static/prism/components/prism-dc.js
@@ -1,10 +1,10 @@
 (function (Prism) {
     Prism.languages.dc = {
-		'comment': /#.*/,
-		'string': /\[[^\n]*\]/,
-		'no_style': /{[^}]*}/,
-		'number': /[0-9A-F]+/,
-		'keyword': /[lLsS]./,
-        'operator': /[xpfcdr?+\-*/%~^|v]/,
+        'comment': /#.*$/,
+        'string': /\[[^\n]*\]/,
+        'no_style': /{[^}]*}/,
+        'number': /[0-9A-F]+/,
+        'keyword': /[lLsS]./,
+        'important': /[xpfcdr?+\-*/%~^|v]/,
     }
 })(Prism);
