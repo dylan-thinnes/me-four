9e554c53a7f658357745a632dbfd1c0631bf8f7e
Dylan Thinnes <dylan.thinnes@protonmail.com>
2019-10-20 16:26:36 +0100

    Fix webtex embedder for long equations

--- get/article/embed-webtex.sh
+++ get/article/embed-webtex.sh
@@ -5,17 +5,18 @@ do
     LINK="${line#src=\"}"
     LINK="${LINK%\"}"
     LATEX="${LINK#*png.latex?}"
+    ID=`echo "$LATEX" | md5sum | cut -d' ' -f1`
     if [[ "$LINK" == "" ]]; then continue; fi
 
     # Memoize previous images
-    if [[ -f "./webtex-hist/$LATEX" ]]; then
-        RESULT=`cat "./webtex-hist/$LATEX"`
+    if [[ -f "./webtex-hist/$ID" ]]; then
+        RESULT=`cat "./webtex-hist/$ID"`
     else
         DATA=$(curl -s "$LINK" | base64 | tr -d "\n")
         RESULT="src=\"data:image/png;base64,$DATA\""
 
         mkdir -p ./webtex-hist
-        echo "$RESULT" > "./webtex-hist/$LATEX"
+        echo "$RESULT" > "./webtex-hist/$ID"
     fi
     INPUT="${INPUT//$line/$RESULT}"
 done < <(echo "$LINKS")
