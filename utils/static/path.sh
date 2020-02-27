3acd2772ec0a7b264ae2ae1192fe6affd67c81a5
Dylan Thinnes <dylan.thinnes@protonmail.com>
2020-02-27 21:46:27 +0000

    Copy over index.html paths verbatim in static/

--- utils/static/path.sh
+++ utils/static/path.sh
@@ -1,5 +1,6 @@
-if [[ -f "$1" ]]
-then
+if echo "$1" | grep -qE "(^|/)index.html$"; then
+    echo "$1"
+elif [[ -f "$1" ]]; then
     TIMESTAMP=`date -r "$1" +%Y%m%d-%H%M%S-%N`
     PATH_EXT="${1##*.}"
     PATH_WITHOUT_EXT="${1%.*}"
